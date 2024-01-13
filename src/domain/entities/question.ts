d7f2b5dd0a26f1539bc407be6f8a283c1c71e8f3
Rafael Cerqueira Gomes <rafahcerqueira@gmail.com>
2024-01-13 16:39:43 -0300

    feat: incluindo novas props nas entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from './value-object/slug'
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface QuestionProps {
+    authorId: UniqueEntityID
+    bestAnswerId?: UniqueEntityID
     title: string
-    content: string
     slug: Slug
-    authorId: string
+    content: string
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
